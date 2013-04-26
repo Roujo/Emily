d3d4e19c663572dca4d9ad2e32557f1bf84d6e9d
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2013-04-25 23:13:25 -0400

    Added User Status ranks, added StringValidator.keepChannels(string[])

--- src/roujo/emily/commands/User.java
+++ src/roujo/emily/commands/User.java
@@ -4,32 +4,59 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum User {
-	Skynet(Status.Super, new String[]{"Skynet", "Skynet|Work"});
-	
-	public enum Status { Super, Regular, BlackListed }
-	
+	Skynet(Status.Super, new String[] { "Skynet", "Skynet|Work" });
+
+	public enum Status {
+		Owner(10), Super(5), Regular(0), BlackListed(-1);
+		
+		private int rank;
+		
+		private Status(int rank) {
+			this.rank = rank;
+		}
+		
+		public int getRank() {
+			return rank;
+		}
+	}
+
 	public static Map<String, User> NICK_MAP = new HashMap<String, User>();
-	
+
 	public static User getUserByNick(String nick) {
 		return NICK_MAP.get(nick);
 	}
-	
-	public static void registerNicks(User user, String[] nicks) {
-		for(String nick : nicks)
-			NICK_MAP.put(nick, user);
+
+	public static boolean isSuper(String nick) {
+		User user = getUserByNick(nick);
+		return user != null && user.status.rank >= Status.Super.rank;
 	}
 	
+	public static boolean isOwner(String nick) {
+		User user = getUserByNick(nick);
+		return user != null && user.status.rank >= Status.Owner.rank;
+	}
+	
+	public static boolean isBlackListed(String nick) {
+		User user = getUserByNick(nick);
+		return user != null && user.status.rank <= Status.BlackListed.rank;
+	}
+
+	public static void registerNicks(User user, String[] nicks) {
+		for (String nick : nicks)
+			NICK_MAP.put(nick, user);
+	}
+
 	private Status status;
 	private String[] nicks;
-	
+
 	private User(Status status, String[] nicks) {
 		this.status = status;
 		this.nicks = nicks;
 		registerNicks(this, nicks);
 	}
-	
+
 	private User(Status status, String nick) {
-		this(status, new String[]{ nick });
+		this(status, new String[] { nick });
 	}
 
 	public Status getStatus() {
--- src/roujo/emily/util/StringValidator.java
+++ src/roujo/emily/util/StringValidator.java
@@ -1,7 +1,20 @@
 package roujo.emily.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringValidator {
 	public static boolean isChannel(String string) {
 		return string.matches("#[#A-z0-9]+");
+	}
+	
+	public static String[] keepChannels(String[] strings) {
+		List<String> channels = new ArrayList<String>();
+		for(String string : strings) {
+			if(isChannel(string))
+				channels.add(string);
+		}
+		String[] channelArray = new String[channels.size()];
+		return channels.toArray(channelArray);
 	}
 }
