eee319d43d7f76be00a16177134e05e7fd45df1a
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2013-04-26 22:14:00 -0400

    CF: Added Roll command.

---
+++ src/roujo/emily/commands/RollCommand.java
@@ -0,0 +1,54 @@
+package roujo.emily.commands;
+
+import java.util.Random;
+
+import roujo.emily.Context;
+import roujo.emily.util.StringValidator;
+
+public class RollCommand extends Command {
+
+	protected RollCommand() {
+		super("roll", "Rolls a certain amount of dice and gives the result.",
+				"dice 2d20 [#channel]", false);
+	}
+
+	@Override
+	public boolean execute(Context context) {
+		if (!isValidSender(context.getSender()))
+			return false;
+
+		String message = context.getMessage();
+		if (message.equals("")) {
+			sendUsageBack(context);
+			return false;
+		}
+
+		String[] args = message.split(" ");
+		if (args[0].matches("[0-9]+d[0-9]+")) {
+			sendUsageBack(context);
+			return false;
+		}
+
+		String[] diceParts = args[0].split("d");
+		int diceNumber = Integer.parseInt(diceParts[0]);
+		int diceType = Integer.parseInt(diceParts[1]);
+		Random random = new Random();
+		int total = 0;
+		for (int i = 0; i < diceNumber; ++i) {
+			total += random.nextInt(diceType) + 1;
+		}
+		String response = "Results of " + args[0] + ": " + total;
+		
+		if(args.length > 1) {
+			if(!StringValidator.isChannel(args[1])) {
+				sendUsageBack(context);
+				return false;
+			}
+			context.getEmily().sendMessage(args[1], response);
+		} else {
+			sendMessageBack(context, response);
+		}
+		return true;
+	}
+
+}
